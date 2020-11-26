725378b19f13358d2f0df0027a7bd3129dbbfc68
Nurutomo <tixbig@gmail.com>
2020-11-27 06:47:16 +0700

    add custom function

---
+++ fixWAPI.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+let dir = './node_modules/@open-wa/wa-automate/dist/api/wapi.js'
+
+const find = /\{ id: "UploadUtils", conditions: \(module\) => (module\.default && module\.default\.encryptAndUpload) \? module\.default : null \}[^,]/
+const replace = `$&
+                { id: "genId", conditions: (module) => (module.default && typeof module.default === 'function' && module.default.toString().match(/crypto/)) ? module.default : null }
+`
+
+text = fs.readFileSync(dir)
+text = text.replace(find, replace)
+
+if (find.test(text)) fs.writeFileSync(dir, text)
+console.log('Modify \'%s\' Done!', dir)
